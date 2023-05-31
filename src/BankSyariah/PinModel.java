bc495cda38088e2a9193b73a9cecb85cf959e870
AlFajri02 <alfajrihuda0211@gmail.com>
2023-05-31 12:39:00 +0700

    UTS_PBO2

---
+++ src/BankSyariah/PinModel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BankSyariah;
+
+/**
+ *
+ * @author win 10
+ */
+public class PinModel {
+   
+    String pin = "021101";
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    
+    
+    
+}
